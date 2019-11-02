22c4715912ce5243c13b9c104f5c1ec58fc84412
Rei Sturm <reisturm@Reis-MacBook-Pro.local>
2019-11-02 16:51:46 -0700

    mentee/mentor schema

---
+++ models/MenteeProfile.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const MenteeProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  website: {
+    type: String
+  },
+  prefcity: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  ethnicity: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  education: [
+    {
+      school: {
+        type: String
+      },
+      degree: {
+        type: String
+      },
+      fieldofstudy: {
+        type: String
+      },
+      from: {
+        type: Date
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', MenteeProfileSchema);
