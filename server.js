30d898e5378fcaafd4b0d2599e4e7e24a3900f2f
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-11-02 14:28:57 -0700

    User authentication routes

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('OPPORTUNITY HACK API RUNNING'));
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
